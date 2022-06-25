529ad383da24ad0b9c9856f98723402b1c9d7556
Kayky Freitas <kayky_defreitas@hotmail.com>
2022-06-24 23:06:41 -0300

    Create microservice

---
+++ src/main/java/com/ms/emailSend/repositories/EmailRepository.java
@@ -0,0 +1,7 @@
+package com.ms.emailSend.repositories;
+
+import com.ms.emailSend.models.EmailModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmailRepository extends JpaRepository<EmailModel, Long> {
+}
